86a4dc9bb1f323caee5f60f02e2a7ebe5de497ed
Nick <melancholic8078@gmail.com>
2026-03-24 19:56:19 +0200

    added final colors and text

--- src/components/ColorComponent/ColorComponent.tsx
+++ src/components/ColorComponent/ColorComponent.tsx
@@ -2,16 +2,22 @@ import css from "./ColorComponent.module.css";
 
 interface Props {
   color: string;
+  name: string;
 }
 
-const ColorComponent = ({ color }: Props) => {
+const ColorComponent = ({ color, name }: Props) => {
   return (
-    <div
-      className={css.circle}
-      style={{
-        background: color,
-      }}
-    ></div>
+    <>
+      <div className={css.color_block}>
+        <div
+          className={css.circle}
+          style={{
+            background: color,
+          }}
+        ></div>
+        <p>{name}</p>
+      </div>
+    </>
   );
 };
 
--- src/components/DressCodeComponent/DressCodeComponent.tsx
+++ src/components/DressCodeComponent/DressCodeComponent.tsx
@@ -3,21 +3,28 @@ import Reveal from "../Reveal/Reveal";
 import ColorComponent from "../ColorComponent/ColorComponent";
 
 const DressCodeComponent = () => {
+  const colors = [
+    { hex: "#FFFFFF", name: "белый" },
+    { hex: "#ede8d0", name: "шампань" },
+    { hex: "#ffffe3", name: "пудровый" },
+    { hex: "#d8cfc4", name: "беж" },
+    { hex: "#c1c6c8", name: "светло-серый" },
+    { hex: "#9ca897", name: "шалфейный" },
+    { hex: "#b0c4c9", name: "пыльно-голубой" },
+  ];
   return (
     <Reveal>
       <div className={css.dress_first_div}>
         <div className={css.dress_second_div}>
-          <h2>Будем рады нарядам в цветах нашей свадьбы</h2>
+          <h2>✨ Dress code ✨</h2>
         </div>
         <img src="/bow.gif" alt="bow" />
         <div className={css.div_color}>
-          <ColorComponent color={"#FFFFFF"} />
-          <ColorComponent color={"#ede8d0"} />
-          <ColorComponent color={"#ffffe3"} />
-          <ColorComponent color={"#ffb2d0"} />
-          <ColorComponent color={"#F7E7CE"} />
-          <ColorComponent color={"chocolate"} />
-          <ColorComponent color={"#F3C064"} />
+          {colors.map((colorObj) => (
+            <div key={colorObj.hex} style={{ marginBottom: "8px" }}>
+              <ColorComponent color={colorObj.hex} name={colorObj.name} />
+            </div>
+          ))}
         </div>
       </div>
     </Reveal>
